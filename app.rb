d6328c0fb0a3ec1f3f607dfe4dd2e841a9b387a4
mgst-d <and8511@ya.ru>
2021-06-24 22:07:35 +0300

    Save to database, add admin

--- app.rb
+++ app.rb
@@ -2,10 +2,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
 #require 'pony'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+get '/admin' do
+	db = SQLite3::Database.new './DataBase/BarberShop.db'
+	db.results_as_hash = true
+	@mh = []
+	db.execute 'select *from users' do |h|
+		@mh << h
+	end
+	erb :admin
 end
 
 get '/about' do
@@ -17,12 +28,12 @@ get '/visit' do
 end
 post '/visit' do
 	@clientname = params[:clientname]
-	@clientfamily = params[:clientfamily]
+	@phone = params[:phone]
 	@visittime = params[:visittime]
 	@choice = params[:choice]
 
 	f = File.open './public/user.txt', 'a'
-	f.write "Имя клиента: #{params[:clientname]}, фамилия: #{params[:clientfamily]}, время визита: #{params[:visittime]}, мастер: #{params[:choice]}, цвет: #{params[:color]}\n"
+	f.write "Имя клиента: #{params[:clientname]}, телефон: #{params[:phone]}, время визита: #{params[:visittime]}, мастер: #{params[:choice]}, цвет: #{params[:color]}\n"
 	f.close
 
 #	Pony.mail(:to => 'and8511@ya.ru', :from => 'my heary', :subject => 'My first mail', :body => "Имя клиента: #{params[:clientname]}, фамилия: #{params[:clientfamily]}, время визита: #{params[:visittime]}, мастер: #{params[:choice]}, цвет: #{params[:color]}\n")
@@ -41,6 +52,14 @@ post '/visit' do
 			return erb :visit
 		end
 	end
+	db = SQLite3::Database.new './DataBase/BarberShop.db'
+	db.execute 'create table if not exists
+	users
+	(id integer primary key autoincrement, Name, Phone, Datestamp, Barber, Color)'
+	db.execute 'insert into users
+	(name, Phone, Datestamp, Barber, Color)
+	values (?,?,?,?,?)', [@clientname, @phone, @visittime, @choice, params[:color]]
+
 erb "<h2>Спасибо! Мы будем Вас ждать!</h2>"
 end
 get '/contacts' do
